f520aa480a23e8e83c7ec8e04729255ed00519cb
Ojas Bendale <ojasbendale18200@gmail.com>
2021-12-29 20:28:04 +0530

    next auth

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,6 +1,6 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-import FacebookProvider from "next-auth/providers/facebook";
+
 export default NextAuth({
   // Configure one or more authentication providers
   providers: [
@@ -8,10 +8,5 @@ export default NextAuth({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET,
     }),
-    FacebookProvider({
-      clientId: process.env.FACEBOOK_ID,
-      clientSecret: process.env.FACEBOOK_SECRET,
-    }),
-    // ...add more providers here
   ],
 });
